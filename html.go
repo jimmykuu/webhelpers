fb7e1ca9d5c10c507ac279963b37445756e052b0
jimmykuu <jimmy.kuu@gmail.com>
2013-07-01 16:47:30 +0800

    replace &nbsp; to space

--- html.go
+++ html.go
@@ -15,6 +15,7 @@ func StripTags(html string) string {
 	html = lowerRe.ReplaceAllStringFunc(html, strings.ToLower)
 	html = strings.Replace(html, "\n", " ", -1)
 	html = strings.Replace(html, "\r", "", -1)
+	html = strings.Replace(html, "&nbsp;", " ", -1)
 	html = brRe.ReplaceAllString(html, "")
 	html = tagRe.ReplaceAllString(html, "")
 	return html

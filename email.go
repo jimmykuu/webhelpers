ba0589177f8a223ee53e91d9d7da1a8ea2776dfa
jimmykuu <jimmy.kuu@gmail.com>
2013-09-04 18:39:34 +0800

    fix send mail no subject error

--- email.go
+++ email.go
@@ -25,6 +25,6 @@ func SendMail(subject string, message string, from string, to []string, smtpConf
 	if isHtml {
 		contentType = "text/html"
 	}
-	msg := fmt.Sprintf(`To: %s\r\nFrom: %s\r\nSubject: %s\r\nContent-Type: %s;charset="utf8"\r\n\r\n%s`, strings.Join(to, ";"), from, subject, contentType, message)
+	msg := fmt.Sprintf("To: %s\r\nFrom: %s\r\nSubject: %s\r\nContent-Type: %s; charset=UTF-8\r\n\r\n%s", strings.Join(to, ";"), from, subject, contentType, message)
 	return smtp.SendMail(smtpConfig.Addr, auth, from, to, []byte(msg))
 }
